dc75fe53bc56c017d559cc7832bfd02f2db366ba
Mehmet Eksi <mehmeteksi99@hotmail.com>
2022-01-19 18:41:20 +0100

    La til resten av modulen

--- del2pakke/del2pakke.go
+++ del2pakke/del2pakke.go
@@ -1,7 +1,19 @@
 package del2pakke
 
-import "rsc.io/quote"
+import (
+	"rsc.io/quote"
+)
 
 func SjekkQuote() string {
-     return quote.Glass() + "\n" +  quote.Go();
+	return quote.Glass() + "\n" + quote.Go()
+}
+
+var tilstand = "[kylling rev korn HS ---\\ \\__/ ______________/---]"
+
+func SjekkTilstand() string {
+	return tilstand
+}
+
+func EndreTilstand() {
+	tilstand = "[ rev korn  ---\\___________________ \\__ kylling HS__/ ____/---]"
 }
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"github.com/youngmemo/demo-is105-oppgave1-del2/del2pakke"
 	"fmt"
+	"github.com/youngmemo/demo-is105-oppgave1-del2/del2pakke"
 )
 
 func main() {
-    fmt.Println(del2pakke.SjekkQuote())
+	fmt.Println(del2pakke.SjekkQuote())
+	fmt.Println(del2pakke.SjekkTilstand())
+	del2pakke.EndreTilstand()
+	fmt.Println("Putter inn HS og kylling i båten")
+	fmt.Println(del2pakke.SjekkTilstand())
+
 }
